c362144c6aa8567879929b51a0d9d54d82b6e125
barriern <nicolas.barrier@ird.fr>
2021-11-19 15:45:39 +0100

    Correct recovering the version

---
+++ ichthyop/__init__.py
@@ -0,0 +1,11 @@
+import pkg_resources  # part of setuptools
+import os
+
+try:
+    __version__ = pkg_resources.require("ichthyop")[0].version
+    print('first version')
+except:
+    VERSION_FILE = os.path.join('{0}/../'.format(os.path.dirname(__file__)), 'VERSION')
+    with open(VERSION_FILE, 'r') as infile:
+        __version__ = infile.read().strip()
+    print('second version')
